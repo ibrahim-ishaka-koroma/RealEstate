48d9c12873415801990b1f27765b7e9767460b2d
ridwan <soriekoroma535@gmail.com>
2024-12-15 16:51:50 +0000

    initial commits

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='pt-10 px-4 md:px-20vlg:px-32 bg-gray-900 w-full overflow-hidden' id='Footer'>
+      <div className='container mx-auto flex flex-col md:flex-row justify-between items-start'>
+        <div className='w-full md:w-1/3 mb-8 md:mb-0'>
+          <img src={assets.logo_dark} alt="" />
+          <p className='text-gray-400 mt-4'>Your dream home is just a click away. Whether buying, selling, or renting, we are here to guide you every step of the way. Contact us today to start your journey</p>
+        </div>
+        <div className='w-full md:w-1/5 mb-8 md:mb-0'>
+          <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
+          <ul className='flex flex-col gap-2 text-gray-400'>
+            <a href="#Header" className='hover:text-white'>Home</a>
+            <a href="#About" className='hover:text-white'>About Us</a>
+            <a href="#Contact" className='hover:text-white'>Contact Us</a>
+            <a href="#" className='hover:text-white'>Privacy and policies</a>
+          </ul>
+        </div>
+        <div className='w-full md:w-1/3'>
+        <h3 className='text-white text-lg font-bold mb-4'>Subscribe to Our newsletter</h3>
+        <p className='text-gray-400  text-lg font-bold mb-4'>
+          the latest news, articles, and resources, sent to your inbox weekly.
+        </p>
+        <div className='flex gap-2'>
+          <input type="email" placeholder='Enter you e-mail' className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto '/>
+          <button className='py-2 px-4 rounded bg-blue-500 text-white'>Subscribe</button>
+        </div>
+        </div>
+      </div>
+      <div className='border-t border-gray-700 py-4 mt-10 text-center text-gray-500'>
+        Copyright 2024 © Ridwan All Right Reserved.
+      </div>
+    </div>
+  )
+}
+
+export default Footer
